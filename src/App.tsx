b1591131d72e80f27597ca6ccc0d0f5ecc2cbbb3
Ryan Vincent Lamaroza <rvlamaroza@gmail.com>
2022-06-18 12:05:28 +0800

    Drafted visuals of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Top10RandomStories from './content/Top10RandomStories';
+import StoryCard from './components/StoryCard';
+import PageFrame from './components/PageFrame';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageFrame>
+      <StoryCard/>
+      <StoryCard/>
+      <StoryCard/>
+      <StoryCard/>
+    </PageFrame>
   );
 }
 
